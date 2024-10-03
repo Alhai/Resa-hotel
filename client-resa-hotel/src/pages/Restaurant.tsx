c598b198912d38a5c3603721e1d31c5065b63d3e
aatma112 <anita.chaudhary98@outlook.com>
2024-10-03 17:07:11 +0200

    Restaurant page updates

---
+++ client-resa-hotel/src/pages/Restaurant.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Restaurant.css'; // Import a CSS file for styling
+import reastaurantDine from '../assets/images/restMain.jpg'; // Image de la dine
+
+// Image-gallary de la dine
+import plat from '../assets/images/plat.jpg'; // Image de la dish
+import poulet from '../assets/images/poulet.jpg'; // Image de la platPoulet
+import table from '../assets/images/table.jpg';
+import chefImage from '../assets/images/chef.jpg';
+
+const Restaurant: React.FC = () => {
+  return (
+    <div className="restaurant-page">
+      <header className="header">
+        <h1>Bienvenue dans Notre Restaurant</h1>
+      </header>
+
+      <div className="content">
+        <div className="description">
+          <h2>À Propos de Nous</h2>
+          <p>
+            Bienvenue dans notre restaurant ! Nous offrons une variété de plats
+            délicieux préparés à partir des ingrédients les plus frais. Nos
+            chefs sont dédiés à vous offrir une expérience culinaire
+            inoubliable. Venez profiter de notre ambiance chaleureuse et de
+            notre service excellent !
+          </p>
+        </div>
+
+        <div className="image-box">
+          <img src={reastaurantDine} alt="Delicious food" />
+        </div>
+      </div>
+
+      <div className="bottom-images">
+        <h3>Nos Plats</h3>
+        <div className="image-gallery">
+          <img src={plat} alt="Dish 1" />
+          <img src={table} alt="Dish 2" />
+          <img src={poulet} alt="Dish 3" />
+          <img src={chefImage} alt="Dish 4" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Restaurant;
