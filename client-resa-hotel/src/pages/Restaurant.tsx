79cf169ab3879865a292e17787d8fa92d2d298ae
aatma112 <anita.chaudhary98@outlook.com>
2024-10-10 18:11:44 +0200

    Changes made in restaurant page

--- client-resa-hotel/src/pages/Restaurant.tsx
+++ client-resa-hotel/src/pages/Restaurant.tsx
@@ -10,35 +10,40 @@ import chefImage from '../assets/images/chef.jpg';
 
 const Restaurant: React.FC = () => {
   return (
-    <div className="restaurant-page">
-      <header className="header">
-        <h1>Bienvenue dans Notre Restaurant</h1>
-      </header>
+    <div className="restaurant-container">
+      {/* Background Image Div */}
+      <div className="background-image"></div>
+      <div
+        className="restaurant-page"
+        // style={{ backgroundImage: `url(${resBackgroundImage})` }}
+      >
+        <header className="header">
+          <h1>Bienvenue dans Notre Restaurant</h1>
+        </header>
+        <div className="content">
+          <div className="description">
+            <h2>À Propos de Nous</h2>
+            <p>
+              Bienvenue dans notre restaurant ! Nous offrons une variété de
+              plats délicieux préparés à partir des ingrédients les plus frais.
+              Nos chefs sont dédiés à vous offrir une expérience culinaire
+              inoubliable. Venez profiter de notre ambiance chaleureuse et de
+              notre service excellent !
+            </p>
+          </div>
 
-      <div className="content">
-        <div className="description">
-          <h2>À Propos de Nous</h2>
-          <p>
-            Bienvenue dans notre restaurant ! Nous offrons une variété de plats
-            délicieux préparés à partir des ingrédients les plus frais. Nos
-            chefs sont dédiés à vous offrir une expérience culinaire
-            inoubliable. Venez profiter de notre ambiance chaleureuse et de
-            notre service excellent !
-          </p>
+          <div className="image-box">
+            <img src={reastaurantDine} alt="Delicious food" />
+          </div>
         </div>
-
-        <div className="image-box">
-          <img src={reastaurantDine} alt="Delicious food" />
-        </div>
-      </div>
-
-      <div className="bottom-images">
-        <h3>Nos Plats</h3>
-        <div className="image-gallery">
-          <img src={plat} alt="Dish 1" />
-          <img src={table} alt="Dish 2" />
-          <img src={poulet} alt="Dish 3" />
-          <img src={chefImage} alt="Dish 4" />
+        <div className="bottom-images">
+          <h3>Nos Plats</h3>
+          <div className="image-gallery">
+            <img src={plat} alt="Dish 1" />
+            <img src={table} alt="Dish 2" />
+            <img src={poulet} alt="Dish 3" />
+            <img src={chefImage} alt="Dish 4" />
+          </div>
         </div>
       </div>
     </div>
