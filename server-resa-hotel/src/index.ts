aa8bb4717ea048deed70f92edfb2a5e46849758e
Alrakys <wilson.beyler@gamil.com>
2024-10-17 17:16:44 +0200

    Finalisation des routes de Chambre

--- server-resa-hotel/src/index.ts
+++ server-resa-hotel/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import chambreRoutes from "./router/chambre.router";
 import userRouter from './router/user.router';
 // const {
 //   TestLoginBDD,
@@ -16,19 +17,22 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/loginTest/:user', async (req, res) => {
+/** Test connexion BDD : **/
+/*app.get('/loginTest/:user', async (req, res) => {
   console.log('Lancement du test connexion BDD');
   try {
-    // const result = await TestLoginBDD(req.params.user);
-    // res.send(result);
+    const result = await TestLoginBDD(req.params.user);
+    res.send(result);
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
   }
-});
+});*/
+
+/** Récupération de l'ensemble des chambres **/
+app.use('/chambres', chambreRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
-
