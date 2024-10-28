1e739b913551cdd721df26ff7601d7366b431deb
Alrakys <wilson.beyler@gamil.com>
2024-10-28 10:32:16 +0100

    possibilité de connexion

--- server-resa-hotel/src/dals/users-dal.ts
+++ server-resa-hotel/src/dals/users-dal.ts
@@ -1,23 +1,31 @@
-import { IUser } from '../interfaces/user-interface';
-import { pool } from '../config/config';
+import {IUser} from '../interfaces/user-interface';
+import {pool} from '../config/config';
+import * as bcrypt from 'bcrypt';
+
 
 export class UserDal {
     constructor() { }
+
     async addUser(user: IUser): Promise<void> {
         const query = `
-            INSERT INTO User (username, email, password, role, created_at)
-            VALUES (?, ?, ?, ?, ?)
-        `;
+        INSERT INTO User (username, email, password, role, created_at)
+        VALUES (?, ?, ?, ?, ?)
+    `;
+
+        // Hacher le mot de passe avec un "salt" (nombre de tours ici fixé à 10 pour l'exemple)
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+
         const values = [
             user.username,
             user.email,
-            user.password,
-            user.role || 'client',  
-            new Date() 
+            hashedPassword, // Utiliser le mot de passe haché
+            user.role || 'client',
+            new Date()
         ];
 
         await pool.query(query, values);
     }
+
     async getAllUsers(): Promise<IUser[]> {
         const query = "SELECT * FROM User";
         const [rows] = await pool.query(query);
@@ -57,4 +65,37 @@ export class UserDal {
         const query = "DELETE FROM User WHERE user_id = ?";
         await pool.query(query, [userId]);
     }
+
+
+    async loginUser(pseudo: string, mdp: string): Promise<IUser | string> {
+        const queryCheckPseudo = 'SELECT * FROM user WHERE username = ?';
+
+        try {
+            const [rows]: any[] = await pool.query(queryCheckPseudo, [pseudo]);
+            if (rows.length === 0) {
+                // Aucun utilisateur trouvé avec le pseudo fourni
+                return 'pseudo_incorrect';
+            }
+
+            const user = rows[0];
+            const isPasswordMatch = await bcrypt.compare(mdp, user.mdp);
+
+            if (!isPasswordMatch) {
+                // Le mot de passe ne correspond pas
+                return 'psw_incorrect';
+            }
+
+            return {
+                id: user.user_id,
+                username: user.username,
+                password: '',
+                email: user.email,
+                role: user.role,
+                createdAt: user.createdAt
+            };
+        } catch (error) {
+            throw new Error(`Erreur lors de la connexion de l'utilisateur : ${(error as Error).message}`);
+        }
+    }
+
 }
